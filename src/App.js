73c2ad3cfdbbb049727d34ebbc469f65d9f59b50
tbrukner <tbrukner@ebay.com>
2020-10-21 00:12:10 +0300

    targil 3

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import TimeShow from './TimeShow';
+import _ from 'lodash';
 
-/* Excrecise 1:
+/*Exercise 1:
 function App() {
   const [value, setValue] = useState('');
 
@@ -23,6 +24,7 @@ function App() {
 }
 */
 
+/*Exercise 2:
 function App() {
   const [timeInSeconds, setTimeInSeconds] = useState(0);
 
@@ -35,6 +37,45 @@ function App() {
       <TimeShow period='Hour' time={timeInSeconds} setTime={setTimeInSeconds}/>
       <TimeShow period='Minutes' time={timeInSeconds} setTime={setTimeInSeconds}/>
       <TimeShow period='Seconds' time={timeInSeconds} setTime={setTimeInSeconds}/>
+    </div>
+  );
+}*/
+
+
+
+
+
+/*Exercise 3*/
+function App() {
+  
+  const [value, setValue] = useState(Math.ceil(Math.random() * 1000));
+  const [result, setResult] = useState();
+
+  function getIsReal() {
+    return Math.random() < 0.9 ? true : false;
+  }
+
+  function check(e) {
+    const isReal=getIsReal();
+    if (isReal && value > e.target.value || !isReal && value < e.target.value) {
+      setResult('Bigger');
+    } else if (isReal && value < e.target.value || !isReal && value > e.target.value) {
+      setResult('Smaller');
+    } else {
+      setResult("You got it !!!!")
+    }
+  }
+
+  function reset () {
+    setValue(Math.ceil(Math.random() * 1000));
+    setResult('');
+  }
+
+  return (
+    <div className="App">
+      <button onClick={reset}>Reset</button>
+      <input type='number' onChange={check}/>
+      <label>{result}</label>
     </div>
   );
 }
